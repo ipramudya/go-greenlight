7f97d0005e2fb1efbb15180191b43cfc63f2848f
ipramudya <ipramudya.dev@gmail.com>
2024-01-30 13:01:51 +0700

    feat(): encoding structs

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/ipramudya/go-greenlight/internal/data"
 )
 
 func (app *application) createMovieHandler(rw http.ResponseWriter, r *http.Request) {
@@ -16,5 +19,18 @@ func (app *application) showMovieHandler(rw http.ResponseWriter, r *http.Request
 		return
 	}
 
-	fmt.Fprintf(rw, "show the details of the movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"romance", "drama", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(rw, movie, http.StatusOK, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(rw, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
---
+++ internal/data/movies.go
@@ -0,0 +1,13 @@
+package data
+
+import "time"
+
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   int32     `json:"runtime,omitempty,string"` // Movie runtime (in minutes)
+	Genres    []string  `json:"genres,omitempty"`         // slices of genres for movie (romance, comedy, etc.)
+	Version   int32     `json:"version"`                  // The version number starts at 1 and will be incremented each
+}
