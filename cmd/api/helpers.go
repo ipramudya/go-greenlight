8cc300b354ee7f5d6aa616d3e2051afa2602fc2d
ipramudya <ipramudya.dev@gmail.com>
2024-01-31 10:04:40 +0700

    feat(): restricting json input

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -52,13 +53,22 @@ func (app *application) writeJSON(rw http.ResponseWriter, data envelope, code in
 }
 
 func (app *application) readJSON(rw http.ResponseWriter, r *http.Request, destination interface{}) error {
-	err := json.NewDecoder(r.Body).Decode(destination)
+	/* limit the size of the request body to 1MB */
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(rw, r.Body, int64(maxBytes))
+
+	/* This means that if the JSON from the client now includes any */
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(destination)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
 		var invalidUnmarshalError *json.InvalidUnmarshalError
 
 		switch {
+
 		/* There is a syntax problem with the JSON being decoded. */
 		case errors.As(err, &syntaxError):
 			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
@@ -78,12 +88,29 @@ func (app *application) readJSON(rw http.ResponseWriter, r *http.Request, destin
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
 
+		/** Decode() will now return an error message in the format "json: unknown field "<name>"
+		 * 	We check for this, extract the field name from the error & interpolate it to custom error message
+		 */
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
 
 		default:
 			return err
 		}
+	}
+
+	/** Call Decode() again, using a pointer to an empty anonymous struct as the destination
+	 *	If the request body only contained a single JSON value this will return an io.EOF error.
+	 * 	So if we get anything else, we know that there is additional data in the request body
+	 * 	and we return our own custom error message.
+	 */
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
 	}
 
 	return nil
