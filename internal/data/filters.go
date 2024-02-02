ea6b93a6bceb7c0abb52d2e7bfe9d61030a80b29
ipramudya <ipramudya.dev@gmail.com>
2024-02-02 07:07:54 +0700

    feat(): sorting lists

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,10 @@
 package data
 
-import "github.com/ipramudya/go-greenlight/internal/validator"
+import (
+	"strings"
+
+	"github.com/ipramudya/go-greenlight/internal/validator"
+)
 
 type Filters struct {
 	Page         int
@@ -15,4 +19,23 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
 	v.Check(f.PageSize <= 100, "page_size", "must be maximum of 100")
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			/* exclude the "-" if given sort value matches our safelist */
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/ipramudya/go-greenlight/internal/validator"
@@ -91,12 +92,14 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 }
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := `
+	query := fmt.Sprintf(`
 		SELECT id, created_at, title, year, runtime, genres, version
 		FROM movies
-			WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
+			WHERE (STRPOS(LOWER(title), LOWER($1)) > 0 OR $1 = '')
 			AND (genres @> $2 OR $2 = '{}')
-		ORDER BY id`
+		ORDER BY %s %s, id ASC`,
+		filters.sortColumn(), filters.sortDirection(),
+	)
 	args := []interface{}{title, pq.Array(genres)}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
